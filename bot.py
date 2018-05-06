c6b03b40870dadc8187af8fd54f1c513c60d75d3
Naomi O'Meara <omeara.naomi@gmail.com>
2018-05-06 14:25:17 -0400

    Comment AND Data

--- bot.py
+++ bot.py
@@ -21,20 +21,27 @@ api = tweepy.API(auth)
 channel_id = 411751 # Change for your sensor
 read_key    = 'BVJS8JTJ7161UVEB'
 
-channel = thingspeak.Channel(id=channel_id,api_key=read_key)
+channel = thingspeak.Channel(id=channel_id,api_key=read_key, fmt='txt')
 
-
+# http://aqicn.org/faq/2013-09-09/revised-pm25-aqi-breakpoints/
+# conversion chart from PM2.5 to quality
+# 0-12: Good
+# 12.1-35.4: Moderate
+# 35.5-55.4: Unhealthy for sensitive groups
+# 55.5-150.4: Unhealthy
+# 150.5-250.4: Very Unhealthy
+# 250.5-500: Hazardous
 
 
 try:
-    # Get the last 2 results from field 1 of your channel
-    print(channel.get_field(field='field1', options = {'results': 2}))
-    # Get the age of the last data in field 1 of your channel in seconds
-    print(channel.get_last_data_age(field='field1'))
-    # Get the last data in field 1 of your channel
-    print(channel.get_field_last(field='field1'))
+    # Get the last 2 results from field 2 of your channel
+    #print(channel.get_field(field='field2', options = {'results': 2}))
+    # Get the age of the last data in field 2 of your channel in seconds
+    #print(channel.get_last_data_age(field='field2'))
+    # Get the last data in field 2 of your channel
+    #print(channel.get_field_last(field='field2'))
     # example tweet: "Particles in the air: blank. Particles can cause blank. If you are blank sensitive be careful. Like golf, the lower the number the better."
-    api.update_status("Too many particles in the air can cause many problems, and make it diffucult to breathe" + channel.get_field_last(field='field1'))
+    api.update_status("Too many particles in the air can cause many problems, and make it difficult to breathe" + channel.get_field_last(field='field2'))
 except:
     raise
     print("connection failed")
